433b0e28879e72284604052291964a8e25d745a6
Priyankaendluri <endlurupriyanka@gmail.com>
2025-07-25 09:29:16 +0530

    Add files via upload

---
+++ spice parser tool .py
@@ -0,0 +1,8 @@
+class SpiceComponent:
+    def __init__(self, name, ctype, nodes, value, raw_line):
+        self.name = name
+        self.type = ctype
+        self.nodes = nodes
+        self.value = value
+        self.raw_line = ra_
+
